eb94293b65d212e29ed72eea2c5f1541391b1853
FlyingCanoe <31600343+FlyingCanoe@users.noreply.github.com>
2019-02-21 12:04:05 -0500

    ajout de operateur
    et comencmend de la simplification

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,11 @@ fn main() {
 
 #[derive(Debug)]
 struct TrueFlot {
-    nominateur: isize,
-    denominateur: usize
+    nominateur: i64,
+    denominateur: i64,
 
 }
-    fn new(n: isize, d: usize) -> TrueFlot {
+    fn new(n: i64, d: i64) -> TrueFlot {
         TrueFlot {
             nominateur: n,
             denominateur: d, 
@@ -40,7 +40,7 @@ impl Div<TrueFlot> for TrueFlot {
 
     fn div(self, other: TrueFlot) -> TrueFlot  {
         TrueFlot {
-            nominateur: self.nominateur * other.denominateur as isize,
+            nominateur: self.nominateur * other.denominateur as i64,
             denominateur: self.denominateur * other.denominateur,
         }
     }
@@ -51,8 +51,8 @@ impl Add<TrueFlot> for TrueFlot {
 
     fn add(self, other: TrueFlot) -> TrueFlot {
         TrueFlot {
-            nominateur: (self.nominateur * other.denominateur as isize) + 
-                        (other.nominateur * self.denominateur as isize),
+            nominateur: (self.nominateur * other.denominateur as i64) + 
+                        (other.nominateur * self.denominateur as i64),
 
             denominateur: self.denominateur * other.denominateur,
         } 
@@ -64,8 +64,8 @@ impl Sub<TrueFlot> for TrueFlot {
 
     fn sub(self, other: TrueFlot) -> TrueFlot {
         TrueFlot {
-            nominateur: (self.nominateur * other.denominateur as isize) - 
-            (other.nominateur * self.denominateur as isize),
+            nominateur: (self.nominateur * other.denominateur as i64) - 
+                        (other.nominateur * self.denominateur as i64),
 
             denominateur: self.denominateur * other.denominateur,
         }
@@ -74,12 +74,24 @@ impl Sub<TrueFlot> for TrueFlot {
 }
 
 impl TrueFlot {
-    fn diviseur(number: isize) -> Vec<usize> {
-        let vec: Vec<usize> = vec!();
+    fn diviseur(number: i64) -> Vec<i64> {
+        let mut vec: Vec<i64> = vec!();
 
         for i in 2..number {
-            println!("{}", number/i);
+            let a = number as f64/i as f64;
+
+            if a.trunc() == a {
+                vec.push(a as i64)
+            }
         }
-        vec
+    vec
+    }
+    fn simplifer(self: mut &self) {
+        let diviseur_nominateur = diviseur(self.nominateur);
+        let diviseur_denominateur = diviseur(self.denominateur);
+
+        for diviseur_nominateur.rev() {
+            
+        }
     }
 }
